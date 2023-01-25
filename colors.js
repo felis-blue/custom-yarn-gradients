14d6e9ec748b9053c719e3748580eee7b786d6d8
felis <felis.blue@gmail.com>
2023-01-25 21:20:26 +0100

    Store colors in map instead of array
    Makes it easier to access the hex for any given color name

--- colors.js
+++ colors.js
@@ -1,7 +1,7 @@
-const colors = [
+const colors = new Map([
     ["petrol", "#345870"],
     ["hell-petrol", "#6495B3"],
     ["pistazie", "#C5EDEA"],
     ["vanille", "#D3CDAC"],
     ["moccha", "#AC7C54"]
-];
+]);

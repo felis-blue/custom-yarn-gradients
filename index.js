54cc7fc0bb67561ff1c62594811472092a104a5e
felis <felis.blue@gmail.com>
2023-02-19 18:12:32 +0100

    refactor and comments

--- index.js
+++ index.js
@@ -14,8 +14,10 @@ function populateColors() {
     let template_div = document.getElementById('color-templates');
     let template = document.querySelector('.color-template');
     template.onclick = handleTemplateClick;
+    template.dataset.color = '000';
     let stylesheet = document.getElementById('styles').sheet;
 
+    // create color template buttons for all colors from colors.js
     for (let [color_num, c] of colors) {
         let new_template = template.cloneNode();
         new_template.dataset.color = color_num;
@@ -24,6 +26,7 @@ function populateColors() {
         new_template.onclick = handleTemplateClick;
         template_div.insertBefore(new_template, template);
 
+        // add the corresponding css rule for the current color
         let new_rule = `[data-color="${color_num}"] {--color: ${c.hex};}`
         stylesheet.insertRule(new_rule);
     }
@@ -37,9 +40,11 @@ function updateAll() {
         return;
     }
 
+    // update resulting weight
     let weight = Math.floor(segment_count * thread_count * 5 / 15) * 10;
     document.getElementById('yarn-weight').innerText = `${weight}g`;
 
+    // update the color segment section
     let color_div = document.getElementById('yarn-colors');
     color_div.replaceChildren();
     for (let segment = 0; segment < segment_count; segment++) {
@@ -62,13 +67,10 @@ function updateAll() {
 function handleTemplateClick(event) {
     color = event.target.dataset.color;
 
+    // color all focused color segments and toggle focus state
     let focused_color_parts = document.querySelectorAll('.color-part.focus');
     for (let part of focused_color_parts) {
-        if (color) {
-            part.dataset.color = color;
-        } else {
-            delete part.dataset.color;
-        }
+        part.dataset.color = color;
         part.classList.toggle('focus');
     }
 
@@ -83,6 +85,7 @@ function createPattern() {
         return;
     }
 
+    // pattern parameters
     let row_repeats = 3;
     let colunm_repeats = 85;
     let template_width = 210;
@@ -92,34 +95,38 @@ function createPattern() {
 
     let color_map = getColorMap();
 
+    // set the svg attributes
     let svg = document.getElementById('svg');
-
     let width = colunm_repeats * template_width + (colunm_repeats - 1) * template_spacing_width;
     let rows_total = segment_count * row_repeats;
     let height = rows_total * template_height + (rows_total - 1) * template_spacing_height;
     svg.setAttribute('viewBox', `0 0 ${width} ${height}`);
 
+    // reset previous pattern
     let group = document.getElementById('path-group');
     group.replaceChildren();
 
+    // create the pattern svg using the existing path templates
     for (let segment = 0; segment < segment_count; segment++) {
         let color_array = [...color_map[segment]];
-
-        for (let j = 0; j < row_repeats; j++) {
-            for (let i = 0; i < colunm_repeats; i++) {
+        for (let row = 0; row < row_repeats; row++) {
+            for (let col = 0; col < colunm_repeats; col++) {
+                // shuffle the colors for each repeat within a segment
                 const shuffled_colors = color_array.sort(() => 0.5 - Math.random());
-
                 for (let thread = 0; thread < thread_count; thread++) {
+                    // create the use tag linking to the correct path template
                     let path = document.createElementNS(svg.namespaceURI, 'use');
                     path.setAttribute('href', `#path-${thread_count}-${thread}`);
 
+                    // fill with color from color shuffle
                     let color = shuffled_colors[thread];
                     if (color != '000') {
                         path.setAttribute('fill', colors.get(color).hex);
                     }
 
-                    let x = i * (template_width + template_spacing_width);
-                    let y = (segment * row_repeats + j) * (template_height + template_spacing_height);
+                    // set the position
+                    let x = col * (template_width + template_spacing_width);
+                    let y = (segment * row_repeats + row) * (template_height + template_spacing_height);
                     path.setAttribute('x', x);
                     path.setAttribute('y', y);
 
@@ -150,6 +157,7 @@ function createPattern() {
 }
 
 function getColorMap() {
+    // create a map of the current colors by segment and row
     let color_map = [];
     let color_parts = document.querySelectorAll('.color-part');
     for (let part of color_parts) {
@@ -162,9 +170,14 @@ function getColorMap() {
 }
 
 function handleColorPartClick(event) {
+    // toggle focus state on normal click
     let toggle_state = event.target.classList.toggle('focus');
 
     if (event.shiftKey) {
+        // toggle focus state of all segments that
+        //  * are on the same row as the clicked segment
+        //  * are before the clicked segment
+        //  * and have the same color as the clicked segment
         let current_row = event.target.dataset.row;
         let color_parts = document.querySelectorAll(`.color-part[data-row="${current_row}"]`);
         for (let part of color_parts) {
